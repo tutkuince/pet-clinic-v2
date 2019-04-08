8df60468e4ba6ad22692cf980301c69be80af60d
tutkuince <tutku.ince@outlook.com>
2019-04-08 19:07:40 +0300

    initial project add

---
+++ src/main/java/com/muditasoft/petclinicv2/PetClinicV2Application.java
@@ -0,0 +1,13 @@
+package com.muditasoft.petclinicv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicV2Application.class, args);
+    }
+
+}
