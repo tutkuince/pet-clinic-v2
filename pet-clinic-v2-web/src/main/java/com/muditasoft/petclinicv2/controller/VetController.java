4424150ea37224128508b4d0c3c0237500ffa71d
tutkuince <tutku.ince@outlook.com>
2019-04-15 22:31:06 +0300

    adding Vet List and updated controller. Closes #15

--- pet-clinic-v2-web/src/main/java/com/muditasoft/petclinicv2/controller/VetController.java
+++ pet-clinic-v2-web/src/main/java/com/muditasoft/petclinicv2/controller/VetController.java
@@ -1,13 +1,24 @@
 package com.muditasoft.petclinicv2.controller;
 
+import com.muditasoft.petclinicv2.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
-    public String listOfVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping(value = {"", "/", "/index", "/index.html"})
+    public String listOfVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
